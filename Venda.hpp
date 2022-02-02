e0e69a88cccb751b79402ac4d24eb14171c65296
Julia Tarchi Maia de Souza <jutarchi00@gmail.com>
2022-02-01 22:46:55 -0300

    Revisão e refatoração

--- Venda.hpp
+++ Venda.hpp
@@ -11,32 +11,13 @@ using namespace std;
 class Venda{
 
     public:
-    double VALOR;
+    double valor;
     string descricao;
     Especialista esp;
     string cliente;
 
 
-    void print() {
-
-        std::cout << "Especialista: ";
-        cout << esp.nome;
-
-        std::cout << " Cliente: ";
-        cout << cliente;
-
-        //std::cout << " : ";
-        //cout << cliente;
-    }
-
-    /*void print() {
-
-        std::cout << "Especialista: ";
-        cout << esp.nome;
-
-        std::cout << " Cliente: ";
-        cout << cliente;
-    }*/
+    void print();
 };
 
 #endif
